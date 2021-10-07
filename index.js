1c2473a610af3c02260a09caf9877da990dcea81
yasar2385 <yasar.mohideen@newgen.co>
2021-10-08 01:08:28 +0530

    4 files modified

--- index.js
+++ index.js
@@ -1,6 +1,62 @@
 // Import stylesheets
 import './style.css';
+import $ from 'jquery';
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+$('.tooltip').mousemove(function (e) {
+  //console.log(e.pageX);
+  //console.log(e.pageY);
+  var TOOL_TOP = document.getElementById('xToolTip');
+  TOOL_TOP.style.left = e.pageX;
+  TOOL_TOP.style.top = e.pageX;
+  TOOL_TOP.style.display = 'inline-block';
+});
+
+(function () {
+  var mousePos;
+
+  document.onmousemove = handleMouseMove;
+  setInterval(getMousePosition, 100); // setInterval repeats every X ms
+
+  function handleMouseMove(event) {
+    var dot, eventDoc, doc, body, pageX, pageY;
+
+    event = event || window.event; // IE-ism
+
+    // If pageX/Y aren't available and clientX/Y are,
+    // calculate pageX/Y - logic taken from jQuery.
+    // (This is to support old IE)
+    if (event.pageX == null && event.clientX != null) {
+      eventDoc = (event.target && event.target.ownerDocument) || document;
+      doc = eventDoc.documentElement;
+      body = eventDoc.body;
+
+      event.pageX =
+        event.clientX +
+        ((doc && doc.scrollLeft) || (body && body.scrollLeft) || 0) -
+        ((doc && doc.clientLeft) || (body && body.clientLeft) || 0);
+      event.pageY =
+        event.clientY +
+        ((doc && doc.scrollTop) || (body && body.scrollTop) || 0) -
+        ((doc && doc.clientTop) || (body && body.clientTop) || 0);
+    }
+
+    mousePos = {
+      x: event.pageX,
+      y: event.pageY,
+    };
+    //console.log('==>' + event.pageX);
+    //console.log('==>' + event.pageY);
+  }
+  function getMousePosition() {
+    var pos = mousePos;
+    if (!pos) {
+      // We haven't seen any movement yet
+    } else {
+      // Use pos.x and pos.y
+    }
+  }
+})();
